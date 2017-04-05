6f8472c439d17de672664cbd59b47bb1f870b3e9
Nicolas Santibañez <nfsantibanez@uc.cl>
2017-04-05 02:31:59 -0300

    update, index, destroy y escribir bien rutas

--- app/controllers/usuarios_controller.rb
+++ app/controllers/usuarios_controller.rb
@@ -1,15 +1,21 @@
 class UsuariosController < ApplicationController
+
 respond_to :json
 skip_before_action :verify_authenticity_token
 
+  # Mostrar el index con todos los usuarios
+  def index
+    usuario = Usuario.all
+    render json: {"usuarios": usuario, "total": usuario.size}
+  end
+
   #Para mostrar un usuario especifico
   def show
-    #respond_with Usuario.find(params[:id])
-    #usuario = Usuario.find(params[:id])
+
     if Usuario.exists?(params[:id])
       respond_with Usuario.find(params[:id])
     else
-      render json: {"error": "Usuario no encontrado"}, status: 404 
+      render json: {"error": "Usuario no encontrado"}, status: 404
     end
 
   end
@@ -31,12 +37,50 @@ skip_before_action :verify_authenticity_token
    end
   end
 
-  private
+  # Actualizar info de usuarios
+   def update
+     #response.headers['Content-Type'] = 'application/json'
 
+     if Usuario.exists?(params[:id])
+       usuario = Usuario.find(params[:id])
+       n_param = JSON.parse(request.body.read)
+
+       #request.request_parameters
+       if n_param["id"]
+         render json: {"error": "id no es modificable"}, status: 400
+       elsif n_param.size > 1
+         render json: {"error": "La modificación ha fallado, solo puedes ingresar un parametro para modificar"}, status: 500
+       elsif usuario.update(usuario_params) && n_param.size == 1
+         if ["usuario", "nombre", "apellido", "twitter"].include?(n_param.keys[0])
+         render json: usuario, status: 200
+         else
+           render json: {"error": "La modificación ha fallado, el atributo entregado no es válido"}, status: 500
+         end
+       else
+         render json: {"error": "La modificación ha fallado"}, status: 500
+       end
+     else
+       render json: {"error": "Usuario no encontrado"}, status: 404
+     end
+   end
+
+   # Borrar usuarios
+     def destroy
+
+       if Usuario.exists?(params[:id])
+         Usuario.find(params[:id]).destroy
+         head 204
+       else
+         render json: {"error": "Usuario no encontrado"}, status: 404
+       end
+     end
+
+
+  private
   #Definir los parametro que son requeridos obligatorios y permitidos
   def usuario_params
-   params.require(:usuario)
-   params.require(:nombre)
+   #params.require(:usuario)
+   #params.require(:nombre)
    params.permit(:usuario, :nombre, :apellido, :twitter)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.htmlsdevise_for :usuarios
-  resources :usuarios,
-  :defaults => { :format => :json },
-  :only => [:show, :create]
+  #resources :usuarios,
+  #:defaults => { :format => :json },
+  #:only => [:index, :show]
+  get 'usuario/' => 'usuarios#index', :defaults => { :format => :json }
+  get 'usuario/:id' => 'usuarios#show', :defaults => { :format => :json }
+  put 'usuario/' => 'usuarios#create', :defaults => { :format => :json }
+  post 'usuario/:id' => 'usuarios#update', :defaults => { :format => :json }
+  delete 'usuario/:id' => 'usuarios#destroy', :defaults => { :format => :json }
+
 end
